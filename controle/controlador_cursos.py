6d6353fe96256496e13f3b02242dcd360b5b2bbf
Davi Chiqueti <davichiqueti@gmail.com>
2024-05-05 14:54:16 -0300

    Update: Controlador e Tela cursos

--- controle/controlador_cursos.py
+++ controle/controlador_cursos.py
@@ -1,5 +1,6 @@
 from entidade.curso import Curso
 from tela.tela_cursos import TelaCursos
+import os
 
 
 class ControladorCursos:
@@ -19,21 +20,62 @@ class ControladorCursos:
     def tela_cursos(self, tela_cursos: TelaCursos):
         self.__tela_cursos = tela_cursos
 
+    def executar_modulo(self):
+        opcoes = {
+            '1': 'Listar Cursos',
+            '2': 'Incluir Curso',
+            '3': 'Alterar Curso',
+            '4': 'Excluir Curso',
+            '10': 'Sair'
+        }
+        opcao_escolhida = str()
+        while True:
+            opcao_escolhida = self.tela_cursos.mostrar_opcoes(opcoes)
+            match opcao_escolhida:
+                case '1':
+                    self.listar_cursos()
+                case '2':
+                    self.incluir_curso()
+                case '3':
+                    listagem_sucedida = self.listar_cursos()
+                    if listagem_sucedida:
+                        self.alterar_curso()
+                case '4':
+                    listagem_sucedida = self.listar_cursos()
+                    if listagem_sucedida:
+                        self.excluir_curso()
+                case '10':
+                    break
+                case _:
+                    self.tela_cursos.mostrar_mensagem('Opção Escolhida Não Existe')
+
     def listar_cursos(self):
-        dados_cursos = list()
-        for curso in self.cursos:
-            dados_cursos.append({
-                'codigo': curso.codigo,
-                'nome': curso.nome
-            })
-        self.tela_cursos.listar_cursos(dados_cursos)
+        listagem_sucedida = False
+        if len(self.cursos) == 0:
+            self.tela_cursos.mostrar_mensagem('Nenhum Curso Cadastrado')
+        else:
+            dados_cursos = list()
+            for curso in self.cursos:
+                dados_cursos.append({
+                    'codigo': curso.codigo,
+                    'nome': curso.nome
+                })
+            self.tela_cursos.listar_cursos(dados_cursos)
+            listagem_sucedida = True
+        return listagem_sucedida
 
     def incluir_curso(self):
-        dados = self.tela_cursos.incluir_curso()
-        codigo = int(dados["codigo"])
-        nome = dados["nome"]
-        novo_curso = Curso(codigo, nome)
-        self.cursos.append(novo_curso)
+        try:
+            dados = self.tela_cursos.incluir_curso()
+            codigo = int(dados["codigo"])
+            nome = dados["nome"]
+            novo_curso = Curso(codigo, nome)
+            self.cursos.append(novo_curso)
+            self.tela_cursos.mostrar_mensagem('Curso cadastrado com sucesso')
+        except:
+            self.tela_cursos.mostrar_mensagem(
+                'Um erro ocorreu durante o cadastro de curso'
+            )
 
     def excluir_curso(self):
         codigo = self.tela_cursos.excluir_curso()
@@ -47,6 +89,10 @@ class ControladorCursos:
         novo_codigo = dados["novo_codigo"]
         novo_nome = dados["novo_nome"]
         indice_curso = self.pesquisar_curso_por_codigo(codigo_antigo)
+        if not indice_curso:
+            self.tela_cursos.mostrar_mensagem(
+                f'Curso com código "{codigo_antigo}" não encontrado'
+            )
         curso = self.cursos[indice_curso]
         if curso:
             if novo_codigo != "":
--- tela/tela_cursos.py
+++ tela/tela_cursos.py
@@ -1,9 +1,9 @@
-import os
+from tela.tela_base import TelaBase
 
 
-class TelaCursos:
+class TelaCursos(TelaBase):
     def __init__(self):
-        self.__controlador_cursos = None
+        super().__init__()
 
     @property
     def controlador_cursos(self):
@@ -13,8 +13,16 @@ class TelaCursos:
     def controlador_cursos(self, controlador_cursos):
         self.__controlador_cursos = controlador_cursos
 
+    def mostrar_opcoes(self, opcoes: dict):
+        self.limpar_tela()
+        print('--- MÓDULO DE CURSOS ---')
+        print('\nOpções disponíveis:')
+        for codigo, acao in opcoes.items():
+            print(f'{codigo} - {acao}')
+        opcao_escolhida = input('\nSelecione uma opção: ')
+        return opcao_escolhida
+
     def listar_cursos(self, dados_cursos: list[dict]):
-        os.system('cls' if os.name=='nt' else 'clear')
         print('--- LISTAGEM DE CURSOS ---\n')
         for curso in dados_cursos:
             codigo = curso["codigo"]
@@ -22,14 +30,13 @@ class TelaCursos:
             print(f"- NOME: {nome}  CÓDIGO: {codigo}")
 
     def incluir_curso(self):
-        os.system('cls' if os.name=='nt' else 'clear')
+        self.limpar_tela()
         print('--- CADASTRO DE CURSO ---\n')
         codigo = input('Código do curso: ')
         nome = input('Nome do curso: ')
         return {'codigo': codigo, 'nome': nome}
 
     def excluir_curso(self):
-        os.system('cls' if os.name=='nt' else 'clear')
         print('--- EXCLUIR CURSO ---\n')
         codigo = input('Código do curso a ser exclúido: ')
         confirmacao = input('\nConfirmar exclusão? (S/N): ')
@@ -40,9 +47,7 @@ class TelaCursos:
             print('Exclusão cancelada')
             return None
 
-
     def alterar_curso(self):
-        os.system('cls' if os.name=='nt' else 'clear')
         print('--- ALTERAR CURSO ---\n')
         codigo_antigo = input('Código do curso a ser alterado: ')
         novo_codigo = input('Novo código: ')
