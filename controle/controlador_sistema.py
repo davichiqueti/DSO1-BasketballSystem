43be9e19bee03c5f4e6038c4cab5888dcd95ad03
Davi Chiqueti <davichiqueti@gmail.com>
2024-05-05 14:54:03 -0300

    Add: Controlador e Tela Sistema

---
+++ controle/controlador_sistema.py
@@ -0,0 +1,33 @@
+from controle.controlador_cursos import ControladorCursos
+from tela.tela_sistema import TelaSistema
+from tela.tela_cursos import TelaCursos
+
+
+class ControladorSistema:
+    def __init__(self):
+        self.__tela_sistema = TelaSistema()
+        self.__controlador_cursos = ControladorCursos()
+        self.__tela_cursos = TelaCursos()
+        self.__controlador_cursos.tela_cursos = self.__tela_cursos
+        self.__tela_cursos.controlador_cursos = self.__controlador_cursos
+
+    @property
+    def tela_sistema(self) -> TelaSistema:
+        return self.__tela_sistema
+
+    @property
+    def controlador_cursos(self) -> ControladorCursos:
+        return self.__controlador_cursos
+
+    def executar_sistema(self):
+        opcoes = {
+            '1': 'Módulo de Cursos',
+            '10': 'Sair'
+        }
+        opcao_escolhida = str()
+        while True:
+            opcao_escolhida = self.tela_sistema.mostrar_opcoes(opcoes)
+            match opcao_escolhida:
+                case '1': self.controlador_cursos.executar_modulo()
+                case '10': break
+                case _: self.tela_sistema.mostrar_mensagem('Opção Escolhida Não Existe')
---
+++ main.py
@@ -0,0 +1,5 @@
+from controle.controlador_sistema import ControladorSistema
+
+
+controlador_sistema = ControladorSistema()
+controlador_sistema.executar_sistema()
