edc40a4d3ae2c88e82e40b1c113874d6df86e347
Davi Chiqueti <davichiqueti@gmail.com>
2024-05-18 12:43:55 -0300

    Add: Entidade Campeonato

---
+++ entidade/campeonato.py
@@ -0,0 +1,46 @@
+from entidade.equipe import Equipe
+from entidade.partida import Partida
+
+
+class Campeonato:
+    def __init__(self, codigo: int, equipes: list[Equipe]):
+        self.__partidas = list()
+        self.__pontuacao = dict()
+        self.__codigo = codigo
+        # Tratamento das equipes
+        if not isinstance(equipes, list):
+            raise TypeError('Campeonato.equipes deve ser do tipo "list"')
+        for item in equipes:
+            if not isinstance(item, Equipe):
+                raise TypeError('Campeonato.equipes só pode conter elementos do tipo "Equipe"')
+        self.__equipes = equipes
+
+    @property
+    def codigo(self) -> int:
+        return self.__codigo
+
+    @property
+    def equipes(self) -> list[Equipe]:
+        return self.__equipes
+
+    @property
+    def partidas(self) -> list[Partida]:
+        return self.__partidas
+
+    def incluir_partida(self, partida: Partida):
+        if isinstance(partida, Partida):
+            self.partidas.append(partida)
+        else:
+            raise TypeError('Campeonato.partidas só pode conter elementos do tipo "Partida"')
+
+    def excluir_partida(self, partida: Partida):
+        if not isinstance(partida, Partida):
+            raise TypeError('Remoção inválida. Campeonato.partidas só cóntem elementos do tipo "Partida"')
+        elif partida not in self.partidas:
+            raise ValueError('Campeonato.partidas não contém a partida a ser removida')
+        else:
+            self.partidas.remove(partida)
+
+    @property
+    def pontuacao(self) -> dict[Equipe, int]:
+        return self.__pontuacao
