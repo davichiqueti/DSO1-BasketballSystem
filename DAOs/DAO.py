0cfbb6635b3abe4f5b43308a341d79ec8b10e00f
Arthur Rodrigues <127230206+ArthurPRodrigues@users.noreply.github.com>
2024-06-18 00:14:09 -0300

    Classe DAO abstrata

---
+++ DAOs/DAO.py
@@ -0,0 +1,49 @@
+import pickle
+from abc import ABC, abstractmethod
+from exceptions.input_incorreto_exception import InputIncorreto
+
+class DAO(ABC):
+    @abstractmethod
+    def __init__(self, datasource=''):
+        self.__datasource = datasource
+        self.__cache = {} #é aqui que vai ficar a lista que estava no controlador. Nesse exemplo estamos usando um dicionario
+        try:
+            self.__load()
+        except FileNotFoundError:
+            self.__dump()
+
+    def __dump(self):
+        pickle.dump(self.__cache, open(self.__datasource, 'wb'))
+
+    def __load(self):
+        self.__cache = pickle.load(open(self.__datasource,'rb'))
+
+    def add(self, key, obj):
+        self.__cache[key] = obj
+        self.__dump()  
+
+    #cuidado: esse update só funciona se o objeto com essa chave já existe
+    def update(self, key, obj):
+        try:
+            if(self.__cache[key] != None):
+                self.__cache[key] = obj #atualiza a entrada
+                self.__dump()  #atualiza o arquivo
+        except KeyError as exc:
+            raise InputIncorreto from exc
+
+    def get(self, key):
+        try:
+            return self.__cache[key]
+        except KeyError as exc:
+            raise InputIncorreto("Chave não encontrada") from exc
+
+    # esse método precisa chamar o self.__dump()
+    def remove(self, key):
+        try:
+            self.__cache.pop(key)
+            self.__dump() #atualiza o arquivo depois de remover um objeto
+        except KeyError as exc:
+            raise InputIncorreto("Chave não encontrada") from exc
+
+    def get_all(self):
+        return self.__cache.values()
