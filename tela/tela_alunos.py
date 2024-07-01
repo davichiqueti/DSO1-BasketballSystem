62f53c18b1874c66538f3bfe98bb600ed9d167e7
Arthur Rodrigues <127230206+ArthurPRodrigues@users.noreply.github.com>
2024-07-01 15:44:24 -0300

    att: tela alunos

--- tela/tela_alunos.py
+++ tela/tela_alunos.py
@@ -1,28 +1,15 @@
-from tela.tela_base import TelaBase
-from datetime import datetime
 import PySimpleGUI as sg
+from datetime import datetime
 
-class TelaAlunos(TelaBase):
+
+class TelaAlunos():
     def __init__(self):
-        super().__init__()
         self.__window = None
         self.init_opcoes()
-
-    def mostra_mensagem(self, msg):
-        sg.popup("", msg)
-
-    def close(self):
-        self.__window.Close()
-
-    def open(self):
-        button, values = self.__window.Read()
-        return button, values
-
 
     def mostrar_opcoes(self):
         self.init_opcoes()
         button, values = self.open()
-
         if values['1']:
             opcao_escolhida = 1
         if values['2']:
@@ -33,7 +20,6 @@ class TelaAlunos(TelaBase):
             opcao_escolhida = 4
         if values['0'] or button in (None, 'Cancelar'):
             opcao_escolhida = 0
-
         self.close()
         return opcao_escolhida
 
@@ -41,59 +27,60 @@ class TelaAlunos(TelaBase):
     def init_opcoes(self):
         sg.ChangeLookAndFeel('DarkTeal4')
         layout = [
-            [sg.Text('-------- Alunos ----------', font=("Helvica", 25))],
+            [sg.Text('-------- ALUNOS ----------', font=("Helvica", 25))],
             [sg.Text('Escolha sua opção', font=("Helvica", 15))],
             [sg.Radio('Incluir Aluno', "RD1", key='1')],
             [sg.Radio('Alterar Aluno', "RD1", key='2')],
             [sg.Radio('Excluir Aluno', "RD1", key='3')],
             [sg.Radio('Listar Alunos', "RD1", key='4')],
             [sg.Radio('Retornar', "RD1", key='0')],
-            [sg.Button('Confirmar'), sg.Cancel('Cancelar')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
         ]
 
         self.__window = sg.Window('Alunos').Layout(layout)
 
-    def incluir_aluno(self):
-        dados_inclusão_aluno = {}
+    def incluir_aluno(self) -> dict:
+        dados_inclusao_aluno = {}
         sg.ChangeLookAndFeel('DarkTeal4')
         layout = [
             [sg.Text('-------- DADOS ALUNO ----------', font=("Helvica", 25))],
-            [sg.Text('Nome:', size=(15, 1)), sg.InputText('', key='Nome')],
-            [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='CPF')],
-            [sg.Text('Data de Nascimento:', size=(15, 1)), sg.InputText('', key='Data de Nascimento')],
-            [sg.Text('Estado:', size=(15, 1)), sg.InputText('', key='Estado')],
-            [sg.Text('Cidade:', size=(15, 1)), sg.InputText('', key='Cidade')],
-            [sg.Text('Bairro:', size=(15, 1)), sg.InputText('', key='Bairro')],
-            [sg.Text('Matricula:', size=(15, 1)), sg.InputText('', key='Matricula')],
-            [sg.Text('Codigo do Curso:', size=(15, 1)), sg.InputText('', key='Codigo do Curso')],
+            [sg.Text('Nome:', size=(15, 1)), sg.InputText('', key='n')],
+            [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
+            [sg.Text('Data de Nascimento:', size=(15, 1)), sg.InputText('', key='dn')],
+            [sg.Text('Estado:', size=(15, 1)), sg.InputText('', key='e')],
+            [sg.Text('Cidade:', size=(15, 1)), sg.InputText('', key='c')],
+            [sg.Text('Bairro:', size=(15, 1)), sg.InputText('', key='b')],
+            [sg.Text('Matricula:', size=(15, 1)), sg.InputText('', key='m')],
+            [sg.Text('Codigo do Curso:', size=(15, 1)), sg.InputText('', key='cc')],
             [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
         ]
-        self.__window = sg.Window('Criação de Alunos').Layout(layout)
+        self.__window = sg.Window('Gestão de Campeonato de Basquete').Layout(layout)
 
         button, values = self.open()
-        nome = values['Nome']
-        cpf = values['CPF']
-        data_nascimento = values['Data de Nascimento']
-        estado = values['Estado']
-        cidade = values['Cidade']
-        bairro = values['Bairro']
-        matricula = values['Matricula']
-        codigo_curso = values['Codigo do Curso']
+        nome = values['n']
+        cpf = values['cpf']
+        data_nascimento = values['dn']
+        estado = values['e']
+        cidade = values['c']
+        bairro = values['b']
+        matricula = values['m']
+        codigo_curso = values['cc']
+
         self.close()
 
         while True:
             if self.verificar_string_alpha(nome) and 4 <= len(nome) <= 60:
                 break
             else:
-                print("nome está incorreto, por favor informe um nome válido")
-                input("Aperte enter para continuar")
+                self.mostra_mensagem("nome está incorreto, por favor informe um nome válido")
+                return self.incluir_aluno()
 
         while True:
             if len(cpf) == 11 and cpf.isnumeric():
                 break
             else:
-                print("CPF está incorreto, por favor informe um CPF válido")
-                input("Aperte ENTER para continuar.")
+                self.mostra_mensagem("CPF está incorreto, por favor informe um CPF válido")
+                return self.incluir_aluno()
 
         while True:
             try:
@@ -101,47 +88,46 @@ class TelaAlunos(TelaBase):
                     data_nascimento, "%d/%m/%Y").date()
                 break
             except ValueError:
-                print(
-                    "Data de nascimento está incorreta, por favor informe uma data no modelo dd/mm/aaaa.")
-                input("Aperte ENTER para continuar.")
+                self.mostra_mensagem("Data de nascimento está incorreta, por favor informe uma data no modelo dd/mm/aaaa.")
+                return self.incluir_aluno()
 
         while True:
             if self.verificar_string_alpha(estado) and 2 <= len(estado) <= 18:
                 break
             else:
-                print("O estado informado está incorreto.")
-                input("Aperte ENTER para continuar.")
+                self.mostra_mensagem("O estado informado está incorreto.")
+                return self.incluir_aluno()
 
         while True:
             if self.verificar_string_alpha(cidade) and 3 <= len(cidade) <= 60:
                 break
             else:
-                print("A cidade informada é inválida.")
-                input("Aperte ENTER para continuar.")
+                self.mostra_mensagem("A cidade informada é inválida.")
+                return self.incluir_aluno()
 
         while True:
             if any(caractere.isalpha() for caractere in bairro) and 4 <= len(bairro) <= 60:
                 break
             else:
-                print("O bairro informado é inválido.")
-                input("Aperte ENTER para continuar.")
+                self.mostra_mensagem("O bairro informado é inválido.")
+                return self.incluir_aluno()
 
         while True:
             if matricula.isnumeric() and len(matricula) == 8:
                 break
             else:
-                print("A matricula informada é inválida.")
-                input("Aperte ENTER para continuar.")
+                self.mostra_mensagem("A matricula informada é inválida.")
+                return self.incluir_aluno()
 
         while True:
             if codigo_curso.isnumeric():
                 codigo_curso = int(codigo_curso)
                 break
             else:
-                print("O código do curso informado é inválida.")
-                input("Aperte ENTER para continuar.")
+                self.mostra_mensagem("O código do curso informado é inválida.")
+                return self.incluir_aluno()
 
-        dados_inclusão_aluno = {
+        dados_inclusao_aluno= {
             "Nome": nome,
             "CPF": cpf,
             "Data de nascimento": data_nascimento,
@@ -152,97 +138,39 @@ class TelaAlunos(TelaBase):
             "codigo do curso": codigo_curso
         }
 
-        return dados_inclusão_aluno
+        return dados_inclusao_aluno
 
-    def alterar_aluno(self):
-        self.limpar_tela()
-        print()
-        print("-------- Insira o CPF do aluno que deseja alterar --------")
+    def alterar_aluno(self) -> dict:
+        dados_aluno_alteracao = dict()
+        sg.ChangeLookAndFeel('DarkTeal4')
+        layout = [
+        [sg.Text('-------- Dados Aluno ----------', font=("Helvica", 25))],
+        [sg.Text('Nome:', size=(15, 1)), sg.InputText('', key='nome')],
+        [sg.Text('Data de Nascimento:', size=(15, 1)), sg.InputText('', key='data_nascimento')],
+        [sg.Text('Estado:', size=(15, 1)), sg.InputText('', key='estado')],
+        [sg.Text('Cidade:', size=(15, 1)), sg.InputText('', key='cidade')],
+        [sg.Text('Bairro:', size=(15, 1)), sg.InputText('', key='bairro')],
+        [sg.Text('Matricula:', size=(15, 1)), sg.InputText('', key='matricula')],
+        [sg.Text('Código do Curso:', size=(15, 1)), sg.InputText('', key='codigo_curso')],
+        [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
 
-        while True:
-            cpf_alteracao = input("cpf: ")
-            if len(cpf_alteracao) == 11 and cpf_alteracao.isdigit():
-                break
-            else:
-                print("CPF está incorreto, por favor informe um CPF válido")
-                input("Aperte ENTER para continuar.")
+        self.__window = sg.Window('Sistema de livros').Layout(layout)
 
-        self.limpar_tela()
-        print('-------- Informe os dados para alteração --------')
+        button, values = self.open()
+        nome = values['nome']
+        data_nascimento = values['data_nascimento']
+        estado = values['estado']
+        cidade = values['cidade']
+        bairro = values['bairro']
+        matricula = values['matricula']
+        codigo_curso = values['codigo_curso']
 
-        while True:
-            cpf = input("cpf: ")
-            if len(cpf) == 11 and cpf.isnumeric():
-                break
-            else:
-                print("CPF está incorreto, por favor informe um CPF válido")
-                input("Aperte ENTER para continuar.")
-
-        while True:
-            nome = input("Nome: ")
-            if self.verificar_string_alpha(nome) and 4 <= len(nome) <= 60:
-                break
-            else:
-                print("nome está incorreto, por favor informe um nome válido")
-                input("Aperte enter para continuar")
-
-        while True:
-            data_nascimento = input("Data de nascimento: ")
-            try:
-                data_nascimento = datetime.strptime(
-                    data_nascimento, "%d/%m/%Y").date()
-                break
-            except ValueError:
-                print(
-                    "Data de nascimento está incorreta, por favor informe uma data no modelo dd/mm/aaaa.")
-                input("Aperte ENTER para continuar.")
-
-        while True:
-            estado = input("Estado: ")
-            if self.verificar_string_alpha(estado) and 2 <= len(estado) <= 18:
-                break
-            else:
-                print("O estado informado está incorreto.")
-                input("Aperte ENTER para continuar.")
-
-        while True:
-            cidade = input("Cidade: ")
-            if self.verificar_string_alpha(cidade) and 3 <= len(cidade) <= 60:
-                break
-            else:
-                print("A cidade informada é inválida.")
-                input("Aperte ENTER para continuar.")
-
-        while True:
-            bairro = input("Bairro: ")
-            if self.verificar_string_alpha(bairro) and 4 <= len(bairro) <= 60:
-                break
-            else:
-                print("O bairro informado é inválido.")
-                input("Aperte ENTER para continuar.")
-
-        while True:
-            matricula = input("Matricula: ")
-            if matricula.isnumeric() and len(matricula) == 8:
-                break
-            else:
-                print("A matricula informada é inválida.")
-                input("Aperte ENTER para continuar.")
-
-        while True:
-            codigo_curso = input("Código do curso: ")
-            if codigo_curso.isnumeric():
-                codigo_curso = int(codigo_curso)
-                break
-            else:
-                print("O código do curso informado é inválida.")
-                input("Aperte ENTER para continuar.")
+        self.close()
 
         # retornando a informação para o controlador
         dados_aluno_alteracao = {
-            "CPF alteracao": cpf_alteracao,
             "Nome": nome,
-            "CPF": cpf,
             "Data de nascimento": data_nascimento,
             "Estado": estado,
             "Cidade": cidade,
@@ -252,49 +180,87 @@ class TelaAlunos(TelaBase):
         }
         return dados_aluno_alteracao
 
-    def excluir_aluno(self):
+    def selecionar_aluno(self):
         sg.ChangeLookAndFeel('DarkTeal4')
         layout = [
-            [sg.Text('-------- EXCLUIR ALUNO ----------', font=("Helvica", 25))],
-            [sg.Text('Matricula:', size=(15, 1)), sg.InputText('', key='Matricula')],
+            [sg.Text('-------- Selecionar Aluno ----------', font=("Helvica", 25))],
+            [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
             [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
         ]
         self.__window = sg.Window('Exclusão de Alunos').Layout(layout)
         button, values = self.open()
-        matricula = values['Matricula']
+        cpf = values['cpf']
         self.close()
         
         while True:
-            if matricula.isnumeric() and len(matricula) == 8:
-                matricula_exclusao = matricula
+            if cpf.isnumeric() and len(cpf) == 11:
+                cpf_selecionado = cpf
                 break
             else:
-                self.mostra_mensagem("Matricula inválida, por favor informe uma matricula válida.")
-                self.excluir_aluno()
+                self.mostra_mensagem("CPF inválido, por favor informe uma CPF válido.")
+                self.init_opcoes()
 
-        return matricula_exclusao
+        return cpf_selecionado
 
-    def listar_alunos(self, dados_alunos: list):
+
+
+    def excluir_aluno(self):
+        sg.ChangeLookAndFeel('DarkTeal4')
+        layout = [
+            [sg.Text('-------- EXCLUIR ALUNO ----------', font=("Helvica", 25))],
+            [sg.Text('CPF:', size=(15, 1)), sg.InputText('', key='cpf')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Exclusão de Alunos').Layout(layout)
+        button, values = self.open()
+        cpf = values['cpf']
+        self.close()
+        
+        while True:
+            if cpf.isnumeric() and len(cpf) == 11:
+                cpf_exclusao = cpf
+                break
+            else:
+                self.mostra_mensagem("CPF inválido, por favor informe uma CPF válido.")
+                self.init_opcoes()
+
+        return cpf_exclusao
+
+    def listar_alunos(self, dados_alunos):
         string_alunos = ""
-
         for novo_aluno in dados_alunos:
-            for dado in novo_aluno:
-                nome = novo_aluno["Nome"]
-                cpf = novo_aluno["CPF"]
-                data_nascimento = novo_aluno["Data de Nascimento"]
-                estado = novo_aluno["estado"]
-                cidade = novo_aluno["cidade"]
-                bairro = novo_aluno["bairro"]
-                matricula = novo_aluno["matricula"]
-                curso = novo_aluno["curso"]
+            nome = str(novo_aluno.get('Nome', ''))
+            cpf = str(novo_aluno.get('CPF', ''))
+            Data_nascimento = str(novo_aluno.get('Data de Nascimento', ''))
+            matricula = str(novo_aluno.get('matricula', ''))
 
-                string_alunos = string_alunos + "Nome do Aluno: " + nome + '\n'
-                string_alunos = string_alunos + "CPF: " + cpf + '\n'
-                string_alunos = string_alunos + "Data de Nascimento: " + data_nascimento + '\n'
-                string_alunos = string_alunos + "Estado: " + estado + '\n'
-                string_alunos = string_alunos + "Cidade: " + cidade + '\n'
-                string_alunos = string_alunos + "Bairro: " + bairro + '\n'
-                string_alunos = string_alunos + "Matricula: " + matricula + '\n'
-                string_alunos = string_alunos + "Curso: " + curso + '\n'
-            
-            sg.Popup('-------- LISTA DE ALUNOS ----------', string_alunos)
+            print(f"Nome: {nome}, CPF: {cpf}, Data de Nascimento: {Data_nascimento}, Matricula: {matricula}")
+
+            string_alunos += "Nome do Aluno: " + nome + '\n'
+            string_alunos += "CPF: " + cpf + '\n'
+            string_alunos += "Data de Nascimento: " + Data_nascimento + '\n'
+            string_alunos += "Matricula: " + matricula + '\n\n'
+
+        sg.Popup('-------- LISTA DE ALUNOS ----------', string_alunos)
+
+
+
+
+    def mostra_mensagem(self, msg):
+        sg.popup("", msg)
+
+    def close(self):
+        self.__window.Close()
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
+    
+
+    def verificar_string_alpha(self, variavel) -> bool:
+        if isinstance(variavel, str):
+            variavel = variavel.split()
+            for caracter in variavel:
+                if not caracter.isalpha():
+                    return False
+            return True
